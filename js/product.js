49fb08be0fcd7544ee1824bb85b7a3ffcc45d610
Abdulrhman El-Daly <abd911daly@gmail.com>
2024-06-09 19:14:29 +0300

    added firebase facebook

---
+++ js/product.js
@@ -0,0 +1,58 @@
+import { addToCart, getCart, removeFromCart } from "./cart.js";
+getCart();
+let params = new URLSearchParams(window.location.search);
+let id = Number(params.get("id"));
+console.log(id);
+let cartIcon = document.getElementById("cartIcon");
+cartIcon.textContent = getCart().length;
+if (id) {
+  const product = JSON.parse(localStorage.getItem("selectedProduct"));
+  if (product && product.id === id) {
+    const quantity = getQuantity(product.id);
+    document.getElementById(
+      "imageDetails"
+    ).innerHTML = ` <img   src="${product.image}" alt="">`;
+    document.getElementById("product-detail").innerHTML = `
+      <h2 class="product-title">${product.title}</h2>
+      <p class="product-price">${product.price}$</p>
+      <h4 class="product-description">${product.description}</h4>
+      <div class="product-action">
+        <div class="product-count">
+          <span><button id="minus"><i class="fa-solid fa-minus"></i></button></span>
+          <span id="quantity-${product.id}">${quantity}</span>
+          <span><button id="plus"><i class="fa-solid fa-plus"></i></button></span>
+        </div>
+      </div>
+    `;
+
+    document.getElementById("plus").addEventListener("click", function () {
+      addToCart(product);
+      updateQuantityDisplay(product.id);
+      console.log(getCart());
+    });
+
+    document.getElementById("minus").addEventListener("click", function () {
+      removeFromCart(product);
+      updateQuantityDisplay(product.id);
+      console.log(getCart());
+    });
+  }
+}
+
+function getQuantity(productId) {
+  const cart = getCart();
+  const productAmount = cart.filter((item) => item.id === productId);
+  return productAmount.length;
+}
+
+function updateQuantityDisplay(productId) {
+  const quantityElement = document.getElementById(`quantity-${productId}`);
+  const cartLength = getCart().length;
+  console.log(cartLength);
+
+  if (quantityElement) {
+    quantityElement.textContent = getQuantity(productId);
+
+    cartIcon.textContent = cartLength;
+  }
+}
--- js/products.js
+++ js/products.js
@@ -4,13 +4,14 @@ export function showProducts(products) {
   let cards = "";
   for (let i = 0; i < products.length; i++) {
     const productId = products[i].id;
-    const quantity = getQuantity(productId);
+
     cards += `<div id="card-${productId}" class="card">
       <img src="${products[i].image}" alt="${products[i].title}">
       <h2>${products[i].title}</h2>
       <h2>Rating: ${products[i].rating.rate}</h2>
       <h2>${products[i].price} $</h2>
-      <h2 id="quantity-${productId}"> Quantity: ${quantity}</h2>
+      <button id="viewProduct-${productId}">View Product</button>
+      
       <button id="addToCart-${productId}" class="addToCart" data-id="${productId}">Add to cart</button>
       <button id="removeFromCart-${productId}" class="removeFromCart" data-id="${productId}">Remove from cart</button>
     </div>`;
@@ -22,7 +23,6 @@ export function showProducts(products) {
       .getElementById(`addToCart-${product.id}`)
       .addEventListener("click", function () {
         addToCart(product);
-        updateQuantityDisplay(product.id);
         console.log(getCart());
       });
 
@@ -33,18 +33,12 @@ export function showProducts(products) {
         updateQuantityDisplay(product.id);
         console.log(getCart());
       });
+
+    document
+      .getElementById(`viewProduct-${product.id}`)
+      .addEventListener("click", function () {
+        localStorage.setItem("selectedProduct", JSON.stringify(product));
+        window.location.href = `product.html?id=${product.id}`;
+      });
   });
-  function getQuantity(productId) {
-    const cart = getCart();
-    const productAmount = cart.filter((item) => item.id === productId);
-
-    return productAmount.length;
-  }
-
-  function updateQuantityDisplay(productId) {
-    const quantityElement = document.getElementById(`quantity-${productId}`);
-    if (quantityElement) {
-      quantityElement.textContent = `Quantity: ${getQuantity(productId)}`;
-    }
-  }
 }
