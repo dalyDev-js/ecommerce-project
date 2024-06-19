00d42560c18857e826de612fa846202d24b51ba3
Abdulrhman El-Daly <abd911daly@gmail.com>
2024-06-19 23:17:25 +0300

    sad

--- shoes/shoes.js
+++ shoes/shoes.js
@@ -1,3 +1,5 @@
+import { addToCart } from "../js/cart.js";
+
 function productTODisplay(product) {
   return ` <div id="item-${product.id}" class="item">
               <div class="item-img">
@@ -5,7 +7,7 @@ function productTODisplay(product) {
                 <div class="splash">
 
                 <div class="addCart">
-                <button id="addToCart-${product.id}" class="addToCart" data-id="${product.id}">Add to Cart</button>
+                <button id="addToCart-${product.id}" class="addToCart" data-id="${product.id}" onclick="addToCartBtn(${product.id})">Add to Cart</button>
                 <h2 id="added-${product.id}"></h2>
                 </div>
 
@@ -27,15 +29,22 @@ function productTODisplay(product) {
                 </div>
                 </div>`;
 }
-
-function displayProducts(product) {
-  let ProductRows = product.map(productTODisplay).join("");
-  document.getElementById("card-items").innerHTML = ProductRows;
+function addToCartBtn(productID) {
+  let products = JSON.parse(localStorage.getItem("products"));
+  let product = products.find((p) => p.id === productID);
+  addToCart(product);
+  console.log(product);
 }
 
+window.addToCartBtn = addToCartBtn;
+
+function displayProducts(products) {
+  let ProductRows = products.map(productTODisplay).join("");
+  document.getElementById("card-items").innerHTML = ProductRows;
+}
 ///////////////////////////////////////////////////////////////////////////////////////////
 function fetchData() {
-  fetch("https://dalydev-js.github.io/shoes")
+  fetch("https://dalydev-js.github.io/shoes/shoes.json")
     .then((res) => res.json())
     .then((data) => {
       if (!localStorage.getItem("products")) {
